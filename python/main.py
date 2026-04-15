c9e3835d010d8f95dd3bd8f32bb71c062fd9c283
nolantuttle <nolantuttle@gmail.com>
2026-04-14 23:24:43 -0700

    Trained new model on cleaned dataset

--- python/main.py
+++ python/main.py
@@ -3,8 +3,8 @@
 #? Project: ResistorClassification
 #?------------------------------------------------------------
 
-import feature_extraction as fe
 import train as tr
 import predict as pr
 
-
+#tr.train_model('archive_clean')
+#pr.predict_resistor('archive/1K_1-4W/1K_1-4W_(2).jpg')
